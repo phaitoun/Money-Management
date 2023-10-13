b28a4d910812fbfee855ea2d995b3ccd870098fe
phaithoun <116714681+phaitoun@users.noreply.github.com>
2023-10-13 11:59:31 +0700

    set up gateway

---
+++ gateway/server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const cors = require('cors');
+const app = express()
+const port = 3000;
+app.use(cors())
+app.use(express.json());
+
+
+app.listen(port , ()=>{
+    console.log(`server is running on localhost:${port}`)
+})
