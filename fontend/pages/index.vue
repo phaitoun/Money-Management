ef347f78050f87134c753969a8fde36d2dbc6ece
SoukSom <129356831+SoukSomVang@users.noreply.github.com>
2023-10-13 13:05:31 +0700

    chage

--- fontend/pages/index.vue
+++ fontend/pages/index.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
     this is home page
+    <p >
+      {{userList.price}}
+      {{userList.action}}
+      {{userList.time}}
+    </p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data(){
+    return{
+      userList : []
+    }
+  },
+  mounted() {
+    // Make an Axios GET request to fetch data from the API endpoint
+    axios
+      .get("http://localhost:3020/api/getAll")
+      .then((response) => {
+        this.userList = response.data;
+        console.log(userList);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  },
 }
 </script>
 
