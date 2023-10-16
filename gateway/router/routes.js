ef860a45164da66cc3958b32b6633eeedff3537e
phaithoun <116714681+phaitoun@users.noreply.github.com>
2023-10-16 23:46:01 +0700

    .

--- gateway/router/routes.js
+++ gateway/router/routes.js
@@ -5,6 +5,6 @@ const userController = require('../controller/userController');
 router.post('/create', userController.createData);
 router.get('/getAction/:action',userController.getAction);// actions = 0 man income, acitons =1 man outcome
 router.get('/getAll',userController.getAll);
-router.delete('/delete', userController.deleteALL)
+router.delete('/delete', userController.deleteALL) //delete all
 
 module.exports = router;
